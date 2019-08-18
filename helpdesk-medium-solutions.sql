fcb849a6c8378addc0bd08b9337763c82d91e9bb
Lily (Yi Yi) Zhang <lily.zhang622@yahoo.com>
2019-08-17 18:35:27 -0700

    Update helpdesk-medium-solutions.sql

--- helpdesk-medium-solutions.sql
+++ helpdesk-medium-solutions.sql
@@ -12,3 +12,25 @@ FROM Customer
   JOIN Issue ON Caller.Caller_id=Issue.Caller_id
 GROUP BY company_name
 HAVING cc > 18
+
+--#7
+/*
+Find the callers who have never made a call. Show first name and last name
+*/
+SELECT first_name, last_name
+FROM Caller 
+  LEFT JOIN Issue ON Caller.caller_id=Issue.caller_id
+WHERE Issue.caller_id IS NULL
+
+--#8
+/*
+For each customer show: Company name, contact name, number of calls where the number of calls is fewer than 5
+*/
+
+SELECT company_name,c.first_name, c.last_name, COUNT(*) as nc
+FROM Customer
+  JOIN Caller ON Customer.company_ref = Caller.company_ref
+  JOIN Issue on Caller.caller_id=Issue.caller_id
+  JOIN Caller c ON Customer.contact_id = c.caller_id
+GROUP BY 1,2,3
+HAVING nc < 5
